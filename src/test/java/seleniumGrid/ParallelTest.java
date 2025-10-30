ba323e7e8d349b90283ab88ce357781ae5da6c9d
pallavipatil0290-glitch <pallavipatil.0290@gmail.com>
2025-10-30 22:23:09 +0530

    Xml file and src file updated

--- src/test/java/seleniumGrid/ParallelTest.java
+++ src/test/java/seleniumGrid/ParallelTest.java
@@ -8,8 +8,8 @@ import java.net.URL;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -21,10 +21,23 @@ public class ParallelTest {
 	@Parameters({"bname"})
   @Test
   public void crossBrowserTest(String bname) throws MalformedURLException, URISyntaxException, InterruptedException {
-		URL serverUrl=new URI("http://localhost:4444/").toURL();
+		URL serverUrl=new URI("http://localhost:4444/wd/hub").toURL();
 		System.out.println("Remote driver connectivity is started!");
 	  if(bname.equals("Chrome")) {
-		   ChromeOptions options=new ChromeOptions();
+		   //ChromeOptions options=new ChromeOptions();
+		   
+		   //new added code
+		   ChromeOptions options = new ChromeOptions();
+		   options.addArguments("--no-sandbox");
+		   options.addArguments("--disable-dev-shm-usage");
+		   options.addArguments("--headless");
+		   options.addArguments("--disable-gpu");
+		   options.addArguments("--remote-allow-origins=*");
+		   options.addArguments("--window-size=1920,1080");
+		   
+		   DesiredCapabilities capabilities = new DesiredCapabilities();
+		   capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+		   options.merge(capabilities);
 		   
 		   driver=new RemoteWebDriver(serverUrl,options);
 		   Utility.getScreenshot(driver);
